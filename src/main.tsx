66bb5adc174e7ce501d8a24f88a04f1756be33af
adarshgb-24 <adarshgb24@gmail.com>
2026-02-27 03:26:39 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,25 @@
 import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client'; // React 18
-// import ReactDOM from 'react-dom'; // Uncomment this if you're using React 17 or below
+//import { createRoot } from 'react-dom/client'; // React 18
+import ReactDOM from 'react-dom'; // Uncomment this if you're using React 17 or below
 
 import App from './App';
 import './index.css';
 
 const rootElement = document.getElementById('root');
 if (rootElement) {
-  const root = createRoot(rootElement); // For React 18
-  root.render(
-    <StrictMode>
-      <App />
-    </StrictMode>
-  );
+//  const root = createRoot(rootElement); // For React 18
+  //root.render(
+    //<StrictMode>
+      //<App />
+    //</StrictMode>
+  //);
+  // For React 17 or below, use the following:
+  ReactDOM.render(
+     <StrictMode>
+       <App />
+     </StrictMode>,
+     rootElement
+   );
 } else {
   console.error('Root element not found');
 }
